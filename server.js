24b44f10ea6015400f203cc5c5524ee2a89d99a7
MinWooByun <yja05092@gmail.com>
2022-12-09 20:39:21 +0900

    Non Nullable Fields
    !를 붙이지 않으면 해당 값이 될 수도 있고 아니면 null 값이 될 수도 있지만 !를 붙이면 null값이 될 수 없다.

--- server.js
+++ server.js
@@ -7,26 +7,29 @@ import { ApolloServer, gql } from "apollo-server";
 // graphql에서 query는 rest API에서 GET request를 만드는 것과 같다.
 const typeDefs = gql`
   type User {
-    id: ID
-    username: String
+    id: ID!
+    username: String!
+    firstName: String!
+    lastName: String
   }
   type Tweet {
-    id: ID
-    text: String
-    author: User
+    id: ID!
+    text: String!
+    author: User!
   }
   type Query {
-    allTweets: [Tweet]
-    tweet(id: ID): Tweet
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
   }
   type Mutation {
-    postTweet(text: String, userId: ID): Tweet
-    deleteTweet(id: ID): Boolean
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
   }
 `;
 // GET /api/v1/tweets
 // GET /api/v1/tweet/:id
 // POST /api/v1/tweets
+// tweet(id: ID!): Tweet에서 마지막 Tweet 옆에 !를 안 붙여준 이유는 id가 존재하지 않으면 리턴 값으로 null를 받을 수도 있기 때문이다.
 
 const server = new ApolloServer({ typeDefs });
 
