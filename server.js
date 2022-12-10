1be82652535dc27c80f6ef383460c4a581a2b1bd
MinWooByun <yja05092@gmail.com>
2022-12-10 15:49:09 +0900

    Query Resolvers
    1. resolvers는 object 형태이어야 하며 서버에서 처리할 수 있다. 이것을 처리하기 위해서는 각 type들의 이름과 type 안에 있는 object들의 이름이 같아야 사용할 수 있다.
    2. arguments가 있는 API는 항상 첫 번째 인자는 root이고 두 번째 인자는 API에서 받아온 arguments이다.

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ import { ApolloServer, gql } from "apollo-server";
 // type을 module로 지정하지 않으면 아래처럼 선언해야 한다.
 // const { ApolloServer, gql } = require("apollo-server");
 
+const tweets = [
+  {
+    id: "1",
+    text: "first one!",
+  },
+  {
+    id: "2",
+    text: "second one!",
+  },
+];
+
 // graphql에서 query는 rest API에서 GET request를 만드는 것과 같다.
 const typeDefs = gql`
   type User {
@@ -15,7 +26,7 @@ const typeDefs = gql`
   type Tweet {
     id: ID!
     text: String!
-    author: User!
+    author: User
   }
   type Query {
     allTweets: [Tweet!]!
@@ -31,7 +42,20 @@ const typeDefs = gql`
 // POST /api/v1/tweets
 // tweet(id: ID!): Tweet에서 마지막 Tweet 옆에 !를 안 붙여준 이유는 id가 존재하지 않으면 리턴 값으로 null를 받을 수도 있기 때문이다.
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    // 함수는 Query type에 있는 object들과 같은 이름으로 함수를 정의해줘야 실행이 된다.
+    allTweets() {
+      return tweets;
+    },
+    // arguments가 있는 API는 항상 첫 번째 인자는 root이고 두 번째 인자는 API에서 받아온 arguments이다.
+    tweet(root, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
